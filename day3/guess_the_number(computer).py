da99fe15d076791ca9ea79fb1fa848442f368009
Wt <1105889424@qq.com>
2022-01-20 19:57:21 +0800

    Today1.20

---
+++ day3/guess_the_number(computer).py
@@ -0,0 +1,16 @@
+import random
+
+def guess(x):
+    random_number =  random.randint(1,x)
+    guess = 0
+    while True:
+        guess = int(input(f"Guess a number between 1 and {x}:"))
+        if guess<random_number:
+            print("Sorry too low")
+        elif guess > random_number:
+            print("Sorry too high")
+        else:
+            print("congadulation")
+            break
+
+guess(10)
